60d07fcae639a348f8539fe1827153febd3a7320
zhongbin pan <zhongbin.pan@hp.com>
2015-05-07 14:37:34 +0800

    Add fake-api with json-server and js faker module

---
+++ fake-apis/js/g.js
@@ -0,0 +1,16 @@
+var R = require('ramda');
+var faker = require('faker');
+var nLoop = R.range(0);
+function aPerson() {
+  return {
+    first: faker.name.firstName(),
+    last: faker.name.lastName(),
+    phone: faker.phone.phoneNumber(),
+    email: faker.internet.email()
+  };
+}
+var data = {
+    person: nLoop(10).map(aPerson)
+};
+require('fs').writeFileSync('../db/db.json', JSON.stringify(data, null, 2), 'utf-8');
+
