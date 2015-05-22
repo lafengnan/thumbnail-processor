48095c243db168fa89dcada9edb3f256b2eb3fa3
zhongbin pan <zhongbin.pan@hp.com>
2015-05-22 21:30:14 +0800

    Add pool support

--- src/main.go
+++ src/main.go
@@ -1,164 +1,142 @@
 package main
 
 import (
-    "log"
-    "time"
-    "flag"
-    "os"
-    "runtime"
-    "syscall"
-    "github.com/garyburd/redigo/redis"
+	"flag"
+	"github.com/garyburd/redigo/redis"
+	"log"
+	"os"
+	"pool"
+	"runtime"
+	"syscall"
+	"time"
 )
 
-func newPool(server, password string) *redis.Pool {
-    return &redis.Pool {
-        MaxIdle: 3,
-        IdleTimeout: 240 * time.Second,
-        Dial: func () (redis.Conn, error) {
-            c, err := redis.Dial("tcp", server)
-            if err != nil {
-                log.Println(err)
-                return nil, err
-            }
-
-            if len(password) > 0 {
-                if _, err :=  c.Do("AUTH", password); err != nil {
-                    c.Close()
-                    log.Println(err)
-                    return nil, err
-                }
-            }
-            return c, err
-        },
-        TestOnBorrow: func (c redis.Conn, t time.Time) error {
-            _, err := c.Do("PING")
-            return err
-        },
-
-    }
-}
-
 var (
-    pool *redis.Pool
-    server = flag.String("server", ":6379", "")
-    password = flag.String("password", "", "")
-    thumbnail = flag.String("file", "", "")
-    key = flag.String("key", "key", "")
-    userid = flag.String("userid", "anan", "")
-    uuid = flag.String("uuid", "123", "")
-    out = flag.String("outfile", "test", "")
+	server    = flag.String("server", ":6379", "")
+	password  = flag.String("password", "", "")
+	thumbnail = flag.String("file", "", "")
+	key       = flag.String("key", "key", "")
+	userid    = flag.String("userid", "anan", "")
+	uuid      = flag.String("uuid", "123", "")
+	out       = flag.String("outfile", "test", "")
 )
 
 func download(conn redis.Conn, userid, uuid, out string) (n int, err error) {
-    k := userid + ":" + uuid
-    b := time.Now()
-    content, err := conn.Do("GET", k)
-    if err != nil {
-        log.Fatal(err)
-    }
-    if content == nil {
-        log.Printf("Empty key: %v", k)
-        syscall.Exit(1)
-    }
-    f, err := os.Create(out)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer f.Close()
+	k := userid + ":" + uuid
+	b := time.Now()
+	content, err := conn.Do("GET", k)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if content == nil {
+		log.Printf("Empty key: %v", k)
+		syscall.Exit(1)
+	}
+	f, err := os.Create(out)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
 
-    n, err = f.Write(content.([]byte))
-    if err != nil {
-        log.Fatal(err)
-    }
-    e := time.Now()
-    log.Printf("Spends %v seconds to download %s from redis", e.Sub(b), k)
-    if n != len(content.([]byte)) {
-        log.Fatal("partial write!")
-    }
+	n, err = f.Write(content.([]byte))
+	if err != nil {
+		log.Fatal(err)
+	}
+	e := time.Now()
+	log.Printf("Spends %v seconds to download %s from redis", e.Sub(b), k)
+	if n != len(content.([]byte)) {
+		log.Fatal("partial write!")
+	}
 
-    return
+	return
 }
 
 func main() {
 
-    flag.Parse()
-    pool = newPool(*server, *password)
-    conn := pool.Get()
-    defer conn.Close()
-    n, err := download(conn, *userid, *uuid, *out)
-    if err != nil {
-        log.Fatal(err)
-    }
-    log.Printf("Download competed! %v bytes was downloaded", n)
-    //daemon(0, 1)
-    /*
-    for {
-        log.Println(syscall.Getpid(), "Starting...")
-        time.Sleep(1 * time.Second)
-    }
-    */
+	flag.Parse()
+	redis_pool := pool.NewRedisPool(*server, *password)
+	conn := redis_pool.Get()
+	defer conn.Close()
+	ppool := pool.NewPool(10)
+	ppool.Run()
+	ppool.AddJob(download, "1", "2", "3")
+
+	/*
+		n, err := download(conn, *userid, *uuid, *out)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("Download competed! %v bytes was downloaded", n)
+	*/
+	//daemon(0, 1)
+	/*
+	   for {
+	       log.Println(syscall.Getpid(), "Starting...")
+	       time.Sleep(1 * time.Second)
+	   }
+	*/
 }
 
 func daemon(nochdir, noclose int) int {
-    var ret, ret2 uintptr
-    var err syscall.Errno
+	var ret, ret2 uintptr
+	var err syscall.Errno
 
-    darwin := runtime.GOOS == "darwin"
+	darwin := runtime.GOOS == "darwin"
 
-    // already a daemon
-    if syscall.Getppid() == 1 {
-        return 0
-    }
+	// already a daemon
+	if syscall.Getppid() == 1 {
+		return 0
+	}
 
-    // fork off the parent process
-    ret, ret2, err = syscall.RawSyscall(syscall.SYS_FORK, 0, 0, 0)
-    if err != 0 {
-        return -1
-    }
+	// fork off the parent process
+	ret, ret2, err = syscall.RawSyscall(syscall.SYS_FORK, 0, 0, 0)
+	if err != 0 {
+		return -1
+	}
 
-    // failure
-    if ret2 < 0 {
-        os.Exit(-1)
+	// failure
+	if ret2 < 0 {
+		os.Exit(-1)
 
-    }
+	}
 
-    // handle exception for darwin
-    if darwin && ret2 == 1 {
-        ret = 0
+	// handle exception for darwin
+	if darwin && ret2 == 1 {
+		ret = 0
 
-    }
+	}
 
-    // if we got a good PID, then we call exit the parent process.
-    if ret > 0 {
-        os.Exit(0)
-    }
+	// if we got a good PID, then we call exit the parent process.
+	if ret > 0 {
+		os.Exit(0)
+	}
 
-    /* Change the file mode mask */
-    _ = syscall.Umask(0)
+	/* Change the file mode mask */
+	_ = syscall.Umask(0)
 
-    // create a new SID for the child process
-    s_ret, s_errno := syscall.Setsid()
-    if s_errno != nil {
-        log.Printf("Error: syscall.Setsid errno: %d", s_errno)
-    }
-    if s_ret < 0 {
-        return -1
-    }
+	// create a new SID for the child process
+	s_ret, s_errno := syscall.Setsid()
+	if s_errno != nil {
+		log.Printf("Error: syscall.Setsid errno: %d", s_errno)
+	}
+	if s_ret < 0 {
+		return -1
+	}
 
-    if nochdir == 0 {
-        os.Chdir("/")
-    }
+	if nochdir == 0 {
+		os.Chdir("/")
+	}
 
-    if noclose == 0 {
-        f, e := os.OpenFile("/dev/null", os.O_RDWR, 0)
-        if e == nil {
-            fd := f.Fd()
-            syscall.Dup2(int(fd), int(os.Stdin.Fd()))
-            syscall.Dup2(int(fd), int(os.Stdout.Fd()))
-            syscall.Dup2(int(fd), int(os.Stderr.Fd()))
-        }
-    }
+	if noclose == 0 {
+		f, e := os.OpenFile("/dev/null", os.O_RDWR, 0)
+		if e == nil {
+			fd := f.Fd()
+			syscall.Dup2(int(fd), int(os.Stdin.Fd()))
+			syscall.Dup2(int(fd), int(os.Stdout.Fd()))
+			syscall.Dup2(int(fd), int(os.Stderr.Fd()))
+		}
+	}
 
-    return 0
+	return 0
 }
- 
--- src/pool/pool.go
+++ src/pool/pool.go
@@ -3,42 +3,108 @@ package pool
 import (
 	"container/list"
 	"fmt"
+	"github.com/garyburd/redigo/redis"
 	"github.com/satori/go.uuid"
 	"log"
+	"reflect"
 	"sync"
 	"time"
 )
 
 type Job struct {
 	Id     uuid.UUID
-	F      func(...interface{}) interface{}
+	Stat   int
+	F      func(...interface{}) (interface{}, error)
 	Args   []interface{}
 	Result interface{}
 	Err    error
 	added  chan bool
 }
 
-type stats struct {
-	Submitted int
-	Running   int
-	Succeed   int
-	Failed    int
+func (job *Job) Info() {
+	fname := reflect.ValueOf(job.F)
+	log.Printf("Job Id: %s, Stat: %d task: %s(%s)", job.Id.String(), job.Stat, fname)
 }
+
+// Constants of a job status
+const (
+	Born = iota
+	Submitted
+	Running
+	Succeed
+	Failed
+	Ghost
+)
 
 type Pool struct {
 	supervisor_started bool
-	worker_started     bool
+	workers_started    bool
 	num_workers        int
+	num_jobs_born      int
 	num_jobs_submitted int
 	num_jobs_running   int
 	num_jobs_succeed   int
 	num_jobs_failed    int
+	num_jobs_ghost     int
+	jobs_inqueue       chan *Job
+	jobs_outqueue      chan *Job
 	jobs_ready_to_run  *list.List
 	jobs_succeed       *list.List
 	jobs_failed        *list.List
 	interval           time.Duration
 	worker_wg          sync.WaitGroup
 	supervisor_wg      sync.WaitGroup
+}
+
+func (pool *Pool) AddJob(f func(...interface{}) (interface{}, error), args ...interface{}) {
+	job := new(Job)
+	job.Id = uuid.NewV4()
+	job.Stat = Born
+	job.F = f
+	job.Args = args
+	job.Result = nil
+	job.Err = nil
+	job.added = make(chan bool)
+
+	pool.jobs_inqueue <- job
+	<-job.added
+	job.Info()
+}
+
+// Start a supervisor
+func (pool *Pool) startSupervisor() {
+	if pool.supervisor_started {
+		panic("Supervisor already started...")
+	}
+	log.Println("Starting supervisor...")
+	pool.supervisor_wg.Add(1)
+	go pool.supervisor()
+	pool.supervisor_wg.Wait()
+	pool.supervisor_started = true
+
+}
+
+// supervisor manage and monitor all jobs
+func (pool *Pool) supervisor() {
+	pool.supervisor_wg.Done()
+}
+
+// Start a worker
+func (pool *Pool) startWorker(idx int) {
+	log.Printf("Starting worker %d ...", idx)
+	pool.worker_wg.Done()
+
+}
+
+// Run pool
+func (pool *Pool) Run() {
+	pool.startSupervisor()
+	for i := 0; i < pool.num_workers; i++ {
+		pool.worker_wg.Add(1)
+		go pool.startWorker(i)
+	}
+	pool.worker_wg.Wait()
+	pool.workers_started = true
 }
 
 func NewPool(workers int) (pool *Pool) {
@@ -50,4 +116,44 @@ func NewPool(workers int) (pool *Pool) {
 	pool.interval = 1
 
 	return
+}
+
+func working(job *Job) (err error) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("Error: Job: %s terminated with : %s", job.Id, err)
+			job.Result = nil
+			job.Err = fmt.Errorf(err.(string))
+		}
+	}()
+
+	job.Result, err = job.F(job.Args...)
+	return
+}
+
+func NewRedisPool(server, password string) *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", server)
+			if err != nil {
+				log.Println(err)
+				return nil, err
+			}
+
+			if len(password) > 0 {
+				if _, err := c.Do("AUTH", password); err != nil {
+					c.Close()
+					log.Println(err)
+					return nil, err
+				}
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
 }
